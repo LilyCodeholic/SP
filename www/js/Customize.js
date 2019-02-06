c60b329cd14a4a1cd61bb6045d9f63bf2bc4851b
LilyCodeholic <46620080+LilyCodeholic@users.noreply.github.com>
2019-02-07 01:46:17 +0900

    add customize update weapon

--- www/js/Customize.js
+++ www/js/Customize.js
@@ -5,23 +5,163 @@ const funcPageCustomize = () =>
         VictoryPose:
         [
             {
-                "pose":"？",
-                "who": "？"
+                "pose":"旗あり",
+                "who": "共通"
+            },
+            {
+                "pose":"旗なし",
+                "who": "共通"
+            },
+            {
+                "pose":"ダブルピース！",
+                "who": "ヒカリ"
+            },
+            {
+                "pose":"ジャンプ！",
+                "who": "レイカ"
+            },
+            {
+                "pose":"大勝利！",
+                "who": "シャーリー"
+            },
+            {
+                "pose":"最高！",
+                "who": "イサドラ"
+            },
+            {
+                "pose":"もっと上を！",
+                "who": "ナギ"
+            },
+            {
+                "pose":"テッペン獲るぜ！",
+                "who": "カズマ"
             },
         ],
         LineColor:
         [
             {
-                "color":"？",
-                "RGB": "#456789"
+                "color":"ヒカリカラー",
+                "RGB": "#ffffff"
             },
+            {
+                "color":"レイカカラー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"シャーリーカラー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"イサドラカラー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ナギカラー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"カズマカラー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"レッド",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"イエロー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ライトグリーン",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"スカイブルー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ウルトラマリン",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ヴァイオレット",
+                "RGB": "#ffffff"
+            }
         ],
         PartsColor:
         [
             {
-                "color":"？",
-                "RGB": "#456789"
+                "color":"デフォルトカラー",
+                "RGB": "#ffffff"
             },
+            {
+                "color":"ワインレッド",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"シャドーエメラルド",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ダークターコイズ",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"シャドーパープル",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ダークレッド",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ダークグリーン",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ダークブルー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ダークイエロー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"レッド",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"イエロー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"クリームイエロー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"グリーン",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ブルー",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ヴァイオレット",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ローズ",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ホワイト",
+                "RGB": "#ffffff"
+            },
+            {
+                "color":"ブラック",
+                "RGB": "#000000"
+            }
         ]
     };
 
